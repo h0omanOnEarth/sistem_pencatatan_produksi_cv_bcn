afe98c75b111dc1d83b6629002dcbb279daa2a65
Clarissa Gracienne <clarissagracia.cg@gmail.com>
2023-11-05 22:00:56 +0700

    Update edit_profil_screen.dart

--- lib/screens/edit_profil_screen.dart
+++ lib/screens/edit_profil_screen.dart
@@ -255,7 +255,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   left: 0,
                   right: 0,
                   child: Container(
-                    color: Colors.black
+                    color: Colors.white
                         .withOpacity(0.3), // Latar belakang semi-transparan
                     child: const Center(
                       child: CircularProgressIndicator(),
